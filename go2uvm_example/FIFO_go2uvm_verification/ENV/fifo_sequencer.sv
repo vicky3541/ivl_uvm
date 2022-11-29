b5e6ddb291c996adb5895b36263c9c709e31c635
Vicky Gheewala <90169492+vicky3541@users.noreply.github.com>
2022-11-29 12:06:48 +0530

    Update fifo_sequencer.sv

--- go2uvm_example/FIFO_go2uvm_verification/ENV/fifo_sequencer.sv
+++ go2uvm_example/FIFO_go2uvm_verification/ENV/fifo_sequencer.sv
@@ -1,9 +1,9 @@
-class fifo_sequencer extends uvm_sequencer#(fifo_trans);
-  
+class fifo_sequencer extends uvm_sequencer #(fifo_seq_item);
+  fifo_seq_item x0;
   `uvm_component_utils(fifo_sequencer)
   
-  function new(string name="", uvm_component parent);
+  function new(string name, uvm_component parent);
     super.new(name, parent);
-  endfunction
+  endfunction 
   
-endclass
+endclass : fifo_sequencer
