e8431ff897e60b6b90d1893d58ac4bc82f9933ca
Vicky Gheewala <90169492+vicky3541@users.noreply.github.com>
2022-11-29 12:06:01 +0530

    Update fifo_env.sv

--- go2uvm_example/FIFO_go2uvm_verification/ENV/fifo_env.sv
+++ go2uvm_example/FIFO_go2uvm_verification/ENV/fifo_env.sv
@@ -1,26 +1,21 @@
-class fifo_enviro extends uvm_env;
-  `uvm_component_utils(fifo_enviro)
+class fifo_env extends uvm_env;
+  fifo_agent fifo_agent1;
   
-  fifo_agent_act		agent_in;
-  fifo_agent_pas		agent_out;
-  fifo_score			score;
+  `uvm_component_utils(fifo_env)
   
-  function new(string name="", uvm_component parent);
-    super.new(name, parent);
+  function new(string name, uvm_component parent);
+    super.new(name,parent);
   endfunction
   
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    agent_in=new("AGENT_IN",this);
-    agent_out=new("AGENT_OUT",this);
-    score=new("scoreboard",this);
-    //`uvm_info(get_type_name(),"environment build",UVM_LOW);
-  endfunction
-  
-  function void connect_phase(uvm_phase phase);
-    super.connect_phase(phase);
-    agent_in.aport.connect(score.sbexport_in);
-    agent_out.aport.connect(score.sbexport_out);
-    //`uvm_info(get_type_name(),"enviro connect phase",UVM_LOW);
-  endfunction
-endclass
+  extern virtual function void build_phase(uvm_phase phase);
+    
+endclass : fifo_env
+    
+    function void fifo_env::build_phase(uvm_phase phase);
+      super.build_phase(phase);
+      fifo_agent1= fifo_agent::type_id::create("fifo_agent1",this);
+      
+      uvm_config_db#(uvm_active_passive_enum)::set(.cntxt(this),.inst_name("*"),.field_name("is_active"),.value(UVM_ACTIVE));
+      
+      
+    endfunction : build_phase 
