f0e2be3c390ee040a6dae07fc5d88b85fdbd8a5d
Vicky Gheewala <90169492+vicky3541@users.noreply.github.com>
2022-11-29 12:13:16 +0530

    Update fifo_trans.sv

--- go2uvm_example/FIFO_go2uvm_verification/ENV/fifo_trans.sv
+++ go2uvm_example/FIFO_go2uvm_verification/ENV/fifo_trans.sv
@@ -4,27 +4,28 @@
 // Company Name : Verifworks PVT LTD Banglore
 // Team Member  : RaviSingh, Shahul, Vicky, Pratibha, Annie
 ///////////////////////////////////////////////////////////////
-`define DATA_WIDTH 8
-`define ADDR_WIDTH 4 
-class fifo_trans extends uvm_sequence_item;
-  function new(string name="");
-    super.new(name);
-  endfunction
-  rand bit [`DATA_WIDTH-1:0] data_in;
-  rand bit rst_n,push,pop;
-  bit [`DATA_WIDTH-1:0] data_out;
-  bit push_err_on_full,pop_err_on_empty, full,empty;
+import fifo_pkg::*;
+
+class fifo_seq_item extends uvm_sequence_item;
   
+  rand logic [DATA_WIDTH-1:0]wr_data;
+  logic [DATA_WIDTH-1:0]rd_data;
+  rand fifo_op kind;
+  bit full,empty,pofe,poee; //poee :- POP ON EMPTY ERROOR, pofe:- PUSH ON FULL ERROR
+  bit [3:0] no_of_rst;
   
-  `uvm_object_utils_begin(fifo_trans)
-  `uvm_field_int(data_in,UVM_ALL_ON)
-  `uvm_field_int(rst_n,UVM_ALL_ON)
-  `uvm_field_int(push,UVM_ALL_ON)
-  `uvm_field_int(pop,UVM_ALL_ON)
-  `uvm_field_int(data_out,UVM_ALL_ON)
-  `uvm_field_int(push_err_on_full,UVM_ALL_ON)
-  `uvm_field_int(pop_err_on_empty,UVM_ALL_ON)
+  `uvm_object_utils_begin(fifo_seq_item)
+  `uvm_field_int(wr_data,UVM_ALL_ON)
+  `uvm_field_int(rd_data,UVM_ALL_ON)
   `uvm_field_int(full,UVM_ALL_ON)
   `uvm_field_int(empty,UVM_ALL_ON)
+  `uvm_field_int(pofe,UVM_ALL_ON)
+  `uvm_field_int(poee,UVM_ALL_ON)
+  `uvm_field_enum(fifo_op,kind,UVM_ALL_ON)
   `uvm_object_utils_end
-endclass
+  
+  function new(string name="fifo_seq_item");
+    super.new(name);
+  endfunction : new
+  
+endclass : fifo_seq_item
